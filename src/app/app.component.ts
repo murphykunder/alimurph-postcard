bef5cd4675e117c59af348a3188947f96b22600d
Murphy Kunder <alisha.kunder.29@gmail.com>
2024-11-22 21:23:02 +0530

    Trigger call to APi on page load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostcardService } from './services/services';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'alimurph-postcard-ui';
+  
+  constructor(
+    private postcardService: PostcardService
+  ){}
+
+  ngOnInit(): void {
+    // backend hosted on render.com which tends to spin down free instances which do not receive requests for more than 15 mins. The free instance takes approx 1 min to start up.
+    // To prevent slowness trigger call to backend when any page loads
+      this.postcardService.getStatus().subscribe({
+        next: (response: any) => console.log(response)
+      })
+  }
 }
--- src/app/modules/card-form/pages/main/main.component.ts
+++ src/app/modules/card-form/pages/main/main.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../../components/navigation/navigation.component';
 import { FooterComponent } from '../../../../components/footer/footer.component';
-import { PostcardService } from '../../../../services/services';
 
 @Component({
   selector: 'app-main',
@@ -11,18 +10,6 @@ import { PostcardService } from '../../../../services/services';
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent implements OnInit{
-
-  constructor(
-    private postcardService: PostcardService
-  ){}
-
-  ngOnInit(): void {
-    // backend hosted on render.com which tends to spin down free instances which do not receive requests for more than 15 mins. The free instance takes approx 1 min to start up.
-    // To prevent slowness trigger call to backend when home page loads
-      this.postcardService.getStatus().subscribe({
-        next: (response) => console.log(response)
-      })
-  }
+export class MainComponent{
 
 }
